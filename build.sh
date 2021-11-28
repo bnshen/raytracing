3c98df7b86ddf809f819ade4b1298b83ac38419a
沈博南 <shenbonan@shenbonandeAir.lan>
2021-11-27 23:57:53 -0800

    v1.1

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rm -rf build
 mkdir build
 cd build
-cmake ..
+CC=/opt/homebrew/bin/gcc-11 CXX=/opt/homebrew/bin/g++-11 cmake ..
 make -j 4
 ./raytracing1 > image.ppm
--- src/main.cc
+++ src/main.cc
@@ -78,9 +78,9 @@ int main() {
     // Image
 
     const auto aspect_ratio = 3.0 / 2.0;
-    const int image_width = 1200;
+    const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 10;
+    const int samples_per_pixel = 50;
     const int max_depth = 10;
 
     // World
@@ -103,6 +103,7 @@ int main() {
 
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        #pragma omp parallel for ordered schedule(dynamic) 
         for (int i = 0; i < image_width; ++i) {
             color pixel_color(0, 0, 0);
             for (int s = 0; s < samples_per_pixel; ++s) {
@@ -111,6 +112,7 @@ int main() {
                 ray r = cam.get_ray(u, v);
                 pixel_color += ray_color(r, world, max_depth);
             }
+            #pragma omp ordered
             write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
